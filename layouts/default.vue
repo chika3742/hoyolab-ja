45fa3ad652039a75ed88e70d58e5dc7f2a562beb
chika3742 <chika3742@outlook.jp>
2021-09-20 16:08:32 +0900

    vercel.json

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,22 @@
       app
     >
       <v-toolbar-title v-text="title" />
+      <v-spacer/>
+      <v-menu offset-y>
+      <template #activator="{on, attrs}">
+        <v-btn v-bind="attrs" text v-on="on"><v-icon>mdi-sort-variant</v-icon>表示</v-btn>
+      </template>
+      <v-list>
+        <v-list-item-group v-model="view" mandatory>
+          <v-list-item value="trend">
+            <v-list-item-title>トレンド</v-list-item-title>
+          </v-list-item>
+          <v-list-item value="new">
+            <v-list-item-title>最新の投稿</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-menu>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -15,7 +31,7 @@
       :absolute="false"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }} chikach / powered by HoYoLAB / <a href="https://github.com/chika3742/hoyolab-ja" target="_blank">Source code</a></span>
+      <span>&copy; {{ new Date().getFullYear() }} chikach / powered by <a href="https://www.hoyolab.com" target="_blank">HoYoLAB</a> / <a href="https://github.com/chika3742/hoyolab-ja" target="_blank">Source code</a></span>
     </v-footer>
   </v-app>
 </template>
@@ -26,6 +42,23 @@ export default {
     return {
       title: 'HoYoLAB 記事一覧ページ (非公式)'
     }
-  }
+  },
+  computed: {
+    view: {
+      get() {
+        return this.$store.getters.view
+      },
+      set(value) {
+        this.$store.commit("setView", value)
+        this.$nuxt?.$emit('changeView', value)
+      },
+    }
+  },
 }
 </script>
+
+<style>
+.cn-select {
+  width: 300px;
+}
+</style>
--- store/index.js
+++ store/index.js
@@ -1,20 +1,26 @@
 export const state = () => ({
   entries: [],
   lastId: undefined,
-  reloadTimes: 0
+  reloadTimes: 0,
+  view: "trend",
 })
 
 export const getters = ({
   entries: (store) => store.entries,
   lastId: (store) => store.lastId,
-  reloadTimes: (store) => store.reloadTimes
+  reloadTimes: (store) => store.reloadTimes,
+  view: (store) => store.view,
 })
 
 export const actions = ({
+  nuxtServerInit({commit}, {route}) {
+    commit("setView", route.query.view ?? "trend")
+  },
   async getEntries({getters, commit}, view) {
     let url = this.$config.baseUrl + "/api/entries?view=" + view
-    if (getters.lastId) url += "&offset=" + getters.lastId
+    if (view == "new" && getters.lastId) url += "&offset=" + getters.lastId
     if (view != "new") url += "&reload_times=" + getters.reloadTimes
+    console.log(url);
     const data = await this.$axios.$get(url)
 
     commit('setLastId', data.last_id)
@@ -29,6 +35,9 @@ export const mutations = ({
   },
   setLastId(store, value) {
     store.lastId = value
+  },
+  setView(store, value) {
+    store.view = value
   },
   incrementReload(store) {
     store.reloadTimes++
