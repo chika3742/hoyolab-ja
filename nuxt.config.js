738347b090f93ed9c7122b7598b7e7cf340cd80b
chika3742 <chika3742@outlook.jp>
2021-09-20 16:16:41 +0900

    Revert "Revert "変更をもとに戻した""
    This reverts commit b180290747ff11364b88f2469cd71be8a7872956.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - hoyolab-ja',
-    title: 'hoyolab-ja',
+    title: 'HoYoLAB 記事一覧',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
